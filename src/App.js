ef3f477ab7343ddf453cce55c77f4cfa1b4e3184
Mahya Asadi <mahyaasadi378@gmail.com>
2023-01-10 13:33:14 +0330

    installed taildwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <div className="flex justify-center">
+        <p className="text-red-300 font-bold">Hellloooooo</p>
+      </div>
       <Routes>
-        
+
       </Routes>
     </div>
   );
