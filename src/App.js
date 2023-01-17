c938fdc8bb8d6467de0b951de8325044e50e263b
amirfazel <amirfazel45@gmail.com>
2023-01-18 02:21:35 +0330

    course page header background image added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
+import Course from './pages/course/course';
+
 function App() {
   return (
     <div className="App">
       <div className="flex justify-center">
-        <p className="text-red-300 font-bold">Hellloooooo</p>
+        {/* <p className="text-red-300 font-bold">Hellloooooo</p> */}
+        <Course></Course>
       </div>
       <Routes>
 
---
+++ src/pages/course/course.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './course.module.css';
+
+
+const Course = () => {
+    return (
+        <>
+        <div className = {` ${classes.hero} h-screen  relative w-screen `} >
+            Hiiiiiii
+        </div>
+        </>
+    );
+}
+
+export default Course;
