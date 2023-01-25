2f2bceb3903072ce0535f300a8fe83dc8372aaa5
amirfazel <amirfazel45@gmail.com>
2023-01-25 14:36:55 +0330

    Info component added

--- src/pages/course/course.js
+++ src/pages/course/course.js
@@ -3,44 +3,28 @@ import classes from "./course.module.css";
 
 import store from './store.json';
 
-import course_logo from "../../assets/course/course-logo.png";
 import teacher_logo from "../../assets/course/teacher.png";
 import pair_programming from '../../assets/course/pair.png';
 
 
 
-import ProgressBar from '../../components/ProgressBar';
+
 import BlackBox from './components/BlackBox/BlackBox';
 import BenefitItem from "./components/BenefitItem/BenefitItem";
 import Description from "./components/Description/Description";
 import PriceModal from "./components/PriceModal/PriceModal";
+import Info from "./components/Info/Info";
 
 
 
 
 const Course = () => {
-  let data = {progress: 60}
+  
   return (
     <>
       {/* hero section */}
       <div className={` ${classes.hero} h-screen relative  `}>
-        <div className="w-1/2 md:w-1/4 h-3/4 flex mx-auto flex-col justify-center items-center">
-          <img src={course_logo} alt="course_logo"></img>
-          <div className="w-full text-white font-bold text-3xl text-center border-b-2 border-gray-500 pb-4">
-            دوره جاوا اسکریپت
-          </div>
-          <div className="w-full text-white flex flex-col items-center md:flex-row justify-between font-bold text-xl">
-            <p>ظرفیت: 59 نفر</p>
-            <p>دانشجو: 53 نفر</p>
-          </div>
-          <div className="w-full h-7 relative bg-gray-300 mt-6 rounded-md overflow-hidden opacity-90 ">
-            {/* <div className="w-4/6 h-full bg-slate-100 flex justify-around items-center">
-              60%
-            </div> */}
-            
-            <ProgressBar data={data} >60%</ProgressBar>
-          </div>
-        </div>
+        <Info />
         <div className="w-full grid grid-cols-3 gap-0 h-1/4 text-white">
           <BlackBox >
             <svg
