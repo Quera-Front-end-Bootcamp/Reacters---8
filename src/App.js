5502fc0dc0838057d866c3b85a4514ca090f2f4d
amirfazel <amirfazel45@gmail.com>
2023-01-28 19:43:39 +0330

    removed unnecessary code in App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <div className="flex justify-center">
-        <p className="text-red-300 font-bold">Hellloooooo</p>
-      </div>
       <Routes>
 
       </Routes>
