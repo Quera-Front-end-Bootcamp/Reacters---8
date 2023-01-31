e94691f293a9e726db35ea322d4ffb100f1f0370
amirfazel <amirfazel45@gmail.com>
2023-02-01 02:07:38 +0330

    updated course constants

--- src/pages/course/components/Rating/Rating.jsx
+++ src/pages/course/components/Rating/Rating.jsx
@@ -1,7 +1,7 @@
 import star from '../../../../assets/course/star.png';
 const Rating = (props) => {
     const rating = [];
-    for (let index = 0; index < props.grade; index++) {
+    for (let index = 0; index < props.rate; index++) {
         rating.push(<span className="w-[15px] h-[15px]"><img src={star} alt="star"></img></span>)
     }
     return (
