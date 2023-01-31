6dbf5aae53fb3262b53386eb022d1f4531b33ffe
Mahya Asadi <mahyaasadi378@gmail.com>
2023-01-31 20:07:12 +0330

    added progessText span

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -3,6 +3,7 @@ const ProgressBar = ({ data }) => {
         <div className="w-48">
             <div className="w-full h-6 bg-gray-200 rounded-md border border-gray-400">
                 <div className="h-full rounded-md bg-[#066649]" style={{ width: data.progress + "%" }}></div>
+                <span className='flex w-full text-sm mt-1'>{data.progress} %</span>
             </div>
         </div >
     );
