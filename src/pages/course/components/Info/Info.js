7702ec906d5eb6aa348f63542e14c331bdaa2656
amirfazel <amirfazel45@gmail.com>
2023-02-02 00:48:32 +0330

    info component data made dynamic

--- src/pages/course/components/Info/Info.js
+++ src/pages/course/components/Info/Info.js
@@ -2,24 +2,20 @@ import course_logo from "../../../../assets/course/course-logo.png";
 import ProgressBar from '../../../../components/ProgressBar';
 
 
-const Info = (props) => {
-    let data = {progress: 60, width:100}
+const Info = ({info}) => {
+    let data = {progress: info.progress, width:100}
     return (
         <div className="w-1/2 md:w-1/4 h-3/4 flex mx-auto flex-col justify-center items-center my-auto">
-          <img className="w-[223px] h-[246px]" src={course_logo} alt="course_logo"></img>
+          <img className="w-[223px] h-[246px]" src={info.heroSrc} alt="course_logo"></img>
           <div className="w-full text-white font-bold text-3xl text-center border-b-2 border-gray-500 pb-4">
-            دوره جاوا اسکریپت
+            {info.title}
           </div>
           <div className="w-full text-white flex flex-col items-center md:flex-row justify-between font-bold text-xl">
-            <p>ظرفیت: {42} نفر</p>
-            <p>دانشجو: {53} نفر</p>
+            <p>ظرفیت: {info.capacity} نفر</p>
+            <p>دانشجو: {info.students_count} نفر</p>
           </div>
           <div className="w-full h-7  mt-6 rounded-md overflow-hidden opacity-90 ">
-            {/* <div className="w-4/6 h-full bg-slate-100 flex justify-around items-center">
-              60%
-            </div> */}
-            
-            <ProgressBar data={data} ></ProgressBar>
+            <ProgressBar data={data} />
           </div>
         </div>
     );
