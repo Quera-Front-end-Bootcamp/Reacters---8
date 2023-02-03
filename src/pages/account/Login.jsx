85dfc9b704eea2dba8ba9d3815d6cd38deffa108
Mahya Asadi <83425903+mahyaasadi@users.noreply.github.com>
2023-02-03 05:13:34 +0330

    responsie login page

--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -1,41 +1,44 @@
 import Wall from "./components/Wall";
+import "./style/login.css"
 
 function LoginPage() {
   return (
-    <div className="page-login flex flex-col-reverse md:flex-row h-screen">
-      <div className="form flex flex-col justify-center items-center flex-1">
+    <div className="page-login flex flex-col-reverse md:flex-row h-screen text-[#0b686e]">
+      <div className="form flex flex-col justify-center items-center flex-1 md:overflw-hidden overflow-scroll pt-32">
         <div className="w-9/12">
-          <p className="text-4xl text-blue-800 text-right">ورود کاربر</p>
-            <div className="flex flex-row-reverse text-input bg-gray-100 p-3 my-4">
-            <label for="form-email" className="text-blue-800">: ایمیل</label>
-      <input
-        type="email"
-        id="form-email"
-        className="flex-1 bg-transparent mx-2 outline-none" 
-      />
-    </div>
-    <div className="flex flex-row-reverse text-input bg-gray-100 p-3 my-4">
-      <label for="form-email" className="text-blue-800">
-        : رمز عبور
-      </label>
-      <input
-        type="password"
-        id="form-email"
-        className="flex-1 bg-transparent mx-2 outline-none"
-      />
-    </div>
-          <div className="flex justify-between text-xl text-blue-800 mt-8">
-            <a href="#">فراموشی رمز عبور</a>
-            <p>
-              <input type="checkbox" />
-              مرا به خاطر بسپار
-            </p>
+          <p className="text-3xl font-semibold text-right py-4">ورود کاربر</p>
+          <div className="flex flex-row-reverse text-input bg-gray-100 rounded-md p-3 my-4">
+            <label for="form-email" className="">: ایمیل</label>
+            <input
+              type="email"
+              id="form-email"
+              className="flex-1 bg-transparent mx-2 outline-none w-3/5"
+            />
           </div>
-          <div className="flex mt-8 font-semibold">
-            <button className="mr-4 py-2 px-6 bg-blue-800 text-white">
+          <div className="flex flex-row-reverse text-input bg-gray-100 rounded-md p-3 mb-4">
+            <label for="form-email" className="">
+              : رمز عبور
+            </label>
+            <input
+              type="password"
+              id="form-email"
+              className="flex-1 bg-transparent mx-2 outline-none w-3/5"
+            />
+          </div>
+          <div className="flex lg:flex-row flex-col items-center lg:justify-between gap-1 text-end text-base mt-8">
+            <a href="#" className="w-3/4 lg:block flex justify-center">فراموشی رمز عبور</a>
+            <div className="flex lg:justify-end w-full items-center justify-center">
+              <input type="checkbox" className="w-[10%]" />
+              <p className="rememberCheck lg:w-3/4 md:w-1/2 w-1/4">مرا به خاطر بسپار</p>
+            </div>
+          </div>
+          <div className="flex lg:flex-row lg:justify-between mt-8 font-semibold flex-col items-center">
+            <button className="lg:mr-4 m-0 lg:mb-0 mb-4 py-3 px-4 bg-[#0d5a5f] hover:bg-[#093d41] text-white rounded-md w-[45%]">
               ورود
             </button>
-            <button>ثبت نام</button>
+            <button className="border border-gray-200 rounded-md bg-gray-200 hover:border-[#0d5a5f] py-3 px-4 w-[45%]">
+              ثبت نام
+            </button>
           </div>
         </div>
       </div>
