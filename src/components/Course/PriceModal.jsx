44b76e83f81c1241ee1ffc60098f078c9ac181cc
amirfazel <amirfazel45@gmail.com>
2023-02-15 19:17:31 +0330

    (feat) : course page connected to api

--- src/components/Course/PriceModal.jsx
+++ src/components/Course/PriceModal.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Countdown from "./Countdown";
 
 const PriceField = (props) => {
@@ -10,17 +11,24 @@ const PriceField = (props) => {
 };
 
 const PriceModal = ({ priceInfo }) => {
+
+  priceInfo = { ...priceInfo, discount:60, modalSrc: ""}
+  
+  useEffect(() => {
+    console.log('price', priceInfo.cost);
+  },[])
+
   return (
     <div className="h-[400px] w-[60%] md:min-w bg-slate-100 absolute top-[12%] md:top-[15%] left-[25%] border-2">
       <header className="w-full h-1/4 flex flex-row justify-end items-center gap-2 border-b-2 border-[#0d5a5f] pr-2">
-        <p>{priceInfo.title}</p>
-        <img className="h-12 w-12" src={priceInfo.modalSrc} alt={priceInfo.alt}></img>
+        <p>{`دوره ${priceInfo?.title}`}</p>
+        <img className="h-12 w-12" src={priceInfo?.modalSrc} alt={priceInfo?.alt}></img>
       </header>
       <div className="flex h-3/4 flex-col justify-end flex-wrap px-2">
         <div className="flex flex-col pb-4">
-          <PriceField propKey={'قیمت دوره'} propValue={`${priceInfo.price.toLocaleString("fa")} تومان`} />
-          <PriceField propKey={'تخفیف'} propValue={`${priceInfo.discount.toLocaleString("fa")} %`} />
-          <PriceField propKey={'مبلغ قابل پرداخت'} propValue={`${((priceInfo.price * (100 - priceInfo.discount)) / 100).toLocaleString("fa")} تومان`} />
+          <PriceField propKey={'قیمت دوره'} propValue={`${priceInfo?.cost?.toLocaleString("fa")} تومان`} />
+          <PriceField propKey={'تخفیف'} propValue={`${priceInfo?.discount?.toLocaleString("fa")} %`} />
+          <PriceField propKey={'مبلغ قابل پرداخت'} propValue={`${((priceInfo?.cost * (100 - priceInfo?.discount)) / 100)?.toLocaleString("fa")} تومان`} />
         </div>
         <Countdown />
         <button className="w-1/2 mx-auto my-4 text-sm md:text-base text-white p-2 rounded-md bg-[#0d5a5f] hover:bg-[#093d41]" dir="rtl">ثبت نام در دوره</button>
