7d51cba7357ca9b7af8d21bc73053a501cde8df7
Mahya Asadi <83425903+mahyaasadi@users.noreply.github.com>
2023-02-19 14:42:31 +0330

    added Loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import "./loading.css"
+
+const Loading = () => {
+    return(
+        <div className="loading">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+}
+export default Loading;
