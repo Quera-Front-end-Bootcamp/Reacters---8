df403029b871ca670314e3961bd7b0c4217b8a32
amirfazel <amirfazel45@gmail.com>
2023-02-23 14:18:28 +0330

    ability to delete students completed

--- src/pages/dashboard/students.jsx
+++ src/pages/dashboard/students.jsx
@@ -25,8 +25,9 @@ import {
 import Label from '../../components/label';
 import Iconify from '../../components/iconify';
 import Loading from '../../components/Loading/Loading';
+import EditModal from '../../components/dashboard/students/Deletemodal';
 // sections
-import { UserListHead, UserListToolbar } from '../../sections/@dashboard/students';
+import { UserListHead, UserListToolbar } from '../../components/dashboard/students';
 
 import {AXIOS} from '../../config/axios.config';
 // ----------------------------------------------------------------------
@@ -70,7 +71,13 @@ function applySortFilter(array, comparator, query) {
 }
 
 export default function StudentPage() {
-  
+  const [studentData, setSudentData] = useState({});
+
+  const [DeleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+
+
+  const [EditModalIsOpen, setEditModalIsOpen] = useState(false);
+
   const [loading, setLoading] = useState(false);
 
   const [page, setPage] = useState(0);
@@ -94,19 +101,6 @@ export default function StudentPage() {
   };
 
 
-  const fetchStudents  = async () => {
-    console.log('fetching ...');
-    const res = await AXIOS.get('/api/student/getall', config);
-    setStudents(res.data.result);
-    setLoading(false);
-    console.log(res);
-    } 
-
-  useEffect(()=>{
-    setLoading(true);
-    fetchStudents();
-  }, [])
-
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
@@ -121,22 +115,6 @@ export default function StudentPage() {
     }
     setSelected([]);
   };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-    }
-    setSelected(newSelected);
-  };
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -157,147 +135,183 @@ export default function StudentPage() {
 
   const isNotFound = !filteredUsers.length && !!filterName;
 
+  
+  const fetchStudents  = async () => {
+    const res = await AXIOS.get('/api/student/getall', config);
+    setStudents(res.data.result);
+    setLoading(false);
+    } 
 
-  const deleteStudent = (id, fullName) => {
-    console.log('del  ',id, fullName);
+  const deleteStudent = (id, setDeletingState) => {
     AXIOS.delete(`/api/student/${id}`, config).then(() => {
       fetchStudents();
-      console.log("done");
+      setDeletingState(false);
+      setDeleteModalIsOpen(false);
     });
-    
   }
+
+  
+
+  
+  useEffect(()=>{
+    setLoading(true);
+    fetchStudents();
+  }, [])
 
   return (
     <>
-      {!loading &&<Container>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
-          mb={5}
-        ></Stack>
+      {!loading && (
+        <Container>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb={5}
+          ></Stack>
 
-        <Card>
-          <Typography variant="h4" gutterBottom>
-            User
-          </Typography>
-          <UserListToolbar
-            numSelected={selected.length}
-            filterName={filterName}
-            onFilterName={handleFilterByName}
-          />
+          <Card>
+            <Typography variant="h4" gutterBottom>
+              User
+            </Typography>
+            <UserListToolbar
+              numSelected={selected.length}
+              filterName={filterName}
+              onFilterName={handleFilterByName}
+            />
 
-          <TableContainer sx={{ minWidth: 900 }}>
-            <Table>
-              <UserListHead
-                order={order}
-                orderBy={orderBy}
-                headLabel={TABLE_HEAD}
-                rowCount={students.length}
-                numSelected={selected.length}
-                onRequestSort={handleRequestSort}
-                onSelectAllClick={handleSelectAllClick}
-              />
-              <TableBody>
-                {students
-                  ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row) => {
-                    const { _id, fullName, role, isActive, profile } = row;
-                    const selectedUser = selected.indexOf(fullName) !== -1;
-                    console.log(_id, fullName);
-                    return (
-                      <TableRow
-                        hover
-                        key={_id}
-                        tabIndex={-1}
-                        role="checkbox"
-                        selected={selectedUser}
-                      >
-
-                        <TableCell component="th" scope="row" padding="none">
-                          <Stack
-                            direction="row"
-                            alignItems="center"
-                            spacing={2}
-                          >
-                            <Avatar alt={fullName} src={profile} />
-                            <Typography variant="subtitle2" noWrap>
-                              {fullName}
-                            </Typography>
-                          </Stack>
-                        </TableCell>
-
-                        <TableCell align="left">
-                          <Label color={isActive ? "primary" : "error"}>
-                            {isActive ? "Active" : "deactive"}
-                          </Label>
-                        </TableCell>
-                        <TableCell>
-                          <MenuItem
-                            sx={{ color: "error.main" }}
-                            onClick={() => deleteStudent(_id, fullName)}
-                          >
-                            <Iconify
-                              icon={"eva:trash-2-outline"}
-                              sx={{ mr: 2 }}
-                            />
-                            Delete
-                          </MenuItem>
-                        </TableCell>
-                        <TableCell>
-                          <MenuItem>
-                            <Iconify icon={"eva:edit-fill"} sx={{ mr: 2 }} />
-                            Edit
-                          </MenuItem>
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                {emptyRows > 0 && (
-                  <TableRow style={{ height: 53 * emptyRows }}>
-                    <TableCell colSpan={6} />
-                  </TableRow>
-                )}
-              </TableBody>
-
-              {isNotFound && (
+            <TableContainer sx={{ minWidth: 900 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={students.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                  onSelectAllClick={handleSelectAllClick}
+                />
                 <TableBody>
-                  <TableRow>
-                    <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                      <Paper
-                        sx={{
-                          textAlign: "center",
-                        }}
-                      >
-                        <Typography variant="h6" paragraph>
-                          Not found
-                        </Typography>
+                  {students
+                    ?.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
+                    .map((row) => {
+                      const { _id, fullName, role, isActive, profile, email } = row;
+                      const selectedUser = selected.indexOf(fullName) !== -1;
+                      return (
+                        <TableRow
+                          hover
+                          key={_id}
+                          tabIndex={-1}
+                          role="checkbox"
+                          selected={selectedUser}
+                        >
+                          <TableCell component="th" scope="row" padding="none">
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar
+                                alt={fullName}
+                                src={
+                                  profile !== ""
+                                    ? profile
+                                    : "https://www.shutterstock.com/image-vector/default-avatar-profile-vector-user-260nw-1705357234.jpg"
+                                }
+                              />
+                              <Typography variant="subtitle2" noWrap>
+                                {fullName}
+                              </Typography>
+                            </Stack>
+                          </TableCell>
 
-                        <Typography variant="body2">
-                          No results found for &nbsp;
-                          <strong>&quot;{filterName}&quot;</strong>.
-                          <br /> Try checking for typos or using complete words.
-                        </Typography>
-                      </Paper>
-                    </TableCell>
-                  </TableRow>
+                          <TableCell align="left">
+                            <Label color={isActive ? "primary" : "error"}>
+                              {isActive ? "Active" : "deactive"}
+                            </Label>
+                          </TableCell>
+                          <TableCell>
+                            <MenuItem
+                              sx={{ color: "error.main" }}
+                              onClick={() => {setDeleteModalIsOpen(true); setSudentData({_id, fullName, email});}}
+                            >
+                              <Iconify
+                                icon={"eva:trash-2-outline"}
+                                sx={{ mr: 2 }}
+                              />
+                              Delete
+                            </MenuItem>
+                          </TableCell>
+                          <TableCell>
+                            <MenuItem>
+                              <Iconify icon={"eva:edit-fill"} sx={{ mr: 2 }} />
+                              Edit
+                            </MenuItem>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
                 </TableBody>
-              )}
-            </Table>
-          </TableContainer>
 
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={students.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Card>
-      </Container>}
-      {loading && <div className="flex justify-center items-center"><Loading /></div> }
+                {isNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <Paper
+                          sx={{
+                            textAlign: "center",
+                          }}
+                        >
+                          <Typography variant="h6" paragraph>
+                            Not found
+                          </Typography>
+
+                          <Typography variant="body2">
+                            No results found for &nbsp;
+                            <strong>&quot;{filterName}&quot;</strong>.
+                            <br /> Try checking for typos or using complete
+                            words.
+                          </Typography>
+                        </Paper>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={students.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Card>
+        </Container>
+      )}
+      {loading && (
+        <div className="flex justify-center items-center">
+          <Loading />
+        </div>
+      )}
+      {DeleteModalIsOpen && (
+        <EditModal
+          data={studentData}
+          isOpen={DeleteModalIsOpen}
+          handleClose={() => setDeleteModalIsOpen(false)}
+          handleDelete = {deleteStudent}
+        />
+      )}
     </>
   );
 }
