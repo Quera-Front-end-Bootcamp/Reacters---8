c7ca09eebfa44a5b7954c8db19f89e9d79fe4a17
Mahya Asadi <83425903+mahyaasadi@users.noreply.github.com>
2023-02-28 18:32:53 +0330

    deleted useless files

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -4,7 +4,7 @@ const account = {
   displayName: 'user',
   email: 'user@user.com',
   photoURL: 'https://darbonnewoods.com/wp-content/uploads/2022/04/Blank-Profile.png',
-  role: 'student'
+  role: 'Admin'
 };
 
 export default account;
